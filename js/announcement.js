996b3270fee3c8547984c85859d57e72e619eb3a
CCKNBC <ccknbc@qq.com>
2021-03-21 11:52:33 +0800

    2021/03/21 周日 11:52:32.30: Updated By CCKNBC Local

---
+++ js/announcement.js
@@ -0,0 +1,12 @@
+Snackbar.show({
+    text: '临时公告采用弹窗弹出',
+    backgroundColor: '#696969',
+    textColor: '#8FBC8F',
+    duration: 7e3,
+    pos: 'bottom-center',
+    actionText: '知道了',
+    actionTextColor: '#4CAF50',
+    // onActionClick: function () {
+    //     window.open('http://www.cngongji.cn/', 'newwindow', 'height=480, width=720, left='+ (screen.availWidth-720)/2 +',top=' + (screen.availHeight-480)/2 + ';toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no,status=no')
+    // }
+})
---
+++ js/ccknbc.js
@@ -0,0 +1,67 @@
+// 可爱的Title
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/ccknbc-backup/cdn/logo/logo.png");
+        document.title = '(つェ⊂) 我藏好了哦~~';
+        clearTimeout(titleTime);
+    } else {
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/ccknbc-backup/cdn/logo/favicon.png");
+        document.title = '(*´∇｀*) 被你发现啦~~' + OriginTitle;
+        titleTime = setTimeout(function() {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
+
+if((location.href==="https://ccknbc.github.io/"))
+    {
+        Snackbar.show({
+                pos: 'bottom-center',
+                text: '🔔 您当前访问的是 <a href="http://blog.ccknbc.cc"><font color="#4CAF50"> 博客 </font></a> - <font color="#8fbc8f">Github</font> 镜像站</a>',
+                actionText: '🚀<a href="http://blog.ccknbc.cc"><b>跳转</b></a>',
+                duration: '15000',       
+            });
+}
+
+
+if((location.href==="https://ccknbc.gitlab.io/"))
+{
+    Snackbar.show({
+            pos: 'bottom-center',
+            text: '🔔 您当前访问的是 <a href="http://blog.ccknbc.cc"><font color="#4CAF50"> 博客 </font></a> - <font color="#8fbc8f">Gitlab</font> 镜像站</a>',
+            actionText: '🚀<a href="http://blog.ccknbc.cc"><b>跳转</b></a>',
+            duration: '15000',
+        });
+}
+
+if((location.href==="https://ccknbc.gitee.io/"))
+{
+    Snackbar.show({
+            pos: 'bottom-center',
+            text: '🔔 您当前访问的是 <a href="http://blog.ccknbc.cc"><font color="#4CAF50"> 博客 </font></a> - <font color="#8fbc8f"> Gitee</font> 镜像站</a>',
+            actionText: '🚀<a href="http://blog.ccknbc.cc"><b>跳转</b></a>',
+            duration: '15000',  
+        });
+}
+
+if((location.href==="https://ccknbc.netlify.app/"))
+{
+    Snackbar.show({
+            pos: 'bottom-center',
+            text: '🔔 您当前访问的是 <a href="http://blog.ccknbc.cc"><font color="#4CAF50"> 博客 </font></a> - <font color="#8fbc8f">Netlify</font> 镜像站</a>',
+            actionText: '🚀<a href="http://blog.ccknbc.cc"><b>跳转</b></a>',
+            duration: '15000',
+        });
+}
+
+if((location.href==="https://ccknbc.now.sh/"))
+{
+    Snackbar.show({
+            pos: 'bottom-center',
+            text: '🔔 您当前访问的是 <a href="http://blog.ccknbc.cc"><font color="#4CAF50"> 博客 </font></a> - <font color="#8fbc8f">Vercel</font> 镜像站</a>',
+            actionText: '🚀<a href="http://blog.ccknbc.cc"><b>跳转</b></a>',
+            duration: '15000',  
+        });
+}
