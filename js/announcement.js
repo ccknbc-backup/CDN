7364d40171a27b9f1e343690e92d08f6e47cd155
CCKNBC <ccknbc@qq.com>
2022-05-10 23:34:35 +0800

    2022/05/10 周二 23:34:35.65: Updated By CCKNBC Local

--- js/announcement.js
+++ js/announcement.js
@@ -1,12 +1,12 @@
 Snackbar.show({
-    text: '临时公告采用弹窗弹出',
+    text: '本站统计已更换为【51.LA】',
     backgroundColor: '#696969',
     textColor: '#8FBC8F',
     duration: 7e3,
     pos: 'bottom-center',
-    actionText: '知道了',
+    actionText: '查看隐私政策',
     actionTextColor: '#4CAF50',
-    // onActionClick: function () {
-    //     window.open('http://www.cngongji.cn/', 'newwindow', 'height=480, width=720, left='+ (screen.availWidth-720)/2 +',top=' + (screen.availHeight-480)/2 + ';toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no,status=no')
-    // }
+    onActionClick: function () {
+        window.open('/privacy', 'newwindow', 'height=480, width=720, left='+ (screen.availWidth-720)/2 +',top=' + (screen.availHeight-480)/2 + ';toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no,status=no')
+    }
 })
