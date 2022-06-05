2c755fd46739eee96d21c96b5a98062c63a32afd
CCKNBC <ccknbc@qq.com>
2022-06-05 12:14:53 +0800

    🐛 更换API

--- js/privacy.js
+++ js/privacy.js
@@ -1,23 +1,18 @@
 "use strict";
-
 function getIpInfo() {
-    fetch("https://ip.useragentinfo.com/jsonp").then(function (a) {
-        return a.json()
-    }).then(function (a) {
-        var b = a.ip,
-            c = a.country,
-            d = a.province,
-            e = a.city,
-            f = a.area,
-            g = a.isp;
-            i = a.desc + a.code;
-        document.getElementById("userAgentIp").innerHTML = b, document.getElementById("userAgentCountry").innerHTML =
-            c, document.getElementById("userAgentRegion").innerHTML = d, document.getElementById(
-                "userAgentCity").innerHTML = e, document.getElementById(
-                    "userAgentArea").innerHTML = f, document.getElementById("userAgentIsp").innerHTML = g;
-        var h = navigator.userAgent;
-        document.getElementById("userAgentDevice").innerHTML = h,document.getElementById(
-            "userAgentCode").innerHTML = i;
+    fetch("https://api.ooomn.com/api/ip").then(function (e) {
+        return e.json()
+    }).then(function (e) {
+        var n = e.country,
+            t = e.ip,
+            r = e.province,
+            i = e.city,
+            o = e.isp;
+        document.getElementById("userAgentIp").innerHTML = t, document.getElementById("userAgentCountry").innerHTML =
+            n, document.getElementById("userAgentRegion").innerHTML = r, document.getElementById(
+                "userAgentCity").innerHTML = i, document.getElementById("userAgentIsp").innerHTML = o;
+        var u = navigator.userAgent;
+        document.getElementById("userAgentDevice").innerHTML = u
     })
 }
 getIpInfo();
